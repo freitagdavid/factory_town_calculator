245de1ca8ae3faf310b5de42f82ed97ce8b4b506
David Freitag <freitagdavid@outlook.com>
2019-10-11 12:00:32 -0400

    Getting started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import recipes from "./data/v0.107b"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getTotalNeeded = (itemName, needed) => {
+
 }
 
-export default App;
+function App() {
+  const [state, setState] = useState({ toMake: "default", amount: 0 });
+  const handleChange = e => {
+    setState(state => {
+      return { ...state, [e.target.name]: e.target.value }
+    })
+  }
+  return (
+    <div className='App'>
+      <select onChange={ e => handleChange(e) } value={ state.toMake } name="toMake">
+        <option value='default' disabled>
+          Select Item
+        </option>
+        {
+          Object.entries(recipes).map(item => {
+            return <option value={ item[0] }>{ item[1].name }</option>
+          })
+        })
+      }
+      </select >
+      <input type='number' />
+    </div >
+  )
+}
+
+export default App
---
+++ src/data/v0.107b.js
@@ -0,0 +1,127 @@
+const recipes = {
+    meatStew: {
+        name: "Meat Stew",
+        prodTime: 6,
+        building: "kitchen",
+        ingredients: [
+            {
+                amount: 1,
+                name: 'cookedBeef'
+            },
+            {
+                name: 'potato',
+                amount: 1
+            },
+            {
+                name: 'carrot',
+                amount: 1
+            },
+            {
+                name: 'fuel',
+                amount: 2
+            }
+        ]
+    },
+    cookedBeef: {
+        name: "Cooked Beef",
+        building: "kitchen",
+        prodTime: 5,
+        ingredients: [
+            {
+                amount: 1,
+                name: "beef"
+            },
+            {
+                amount: 1,
+                name: "fuel"
+            }
+        ]
+    },
+    beef: {
+        name: "Beef",
+        building: "pasture",
+        prodTime: 10,
+        ingredients: [
+            {
+                amount: 4,
+                name: "animalFeed"
+            },
+            {
+                amount: 4,
+                name: "water"
+            }
+        ]
+    },
+    airCrystal: {
+        name: "Air Crystal",
+        prodTime: 10,
+        ingredients: [
+            {
+                amount: 4,
+                name: "airEther"
+            },
+            {
+                amount: 2,
+                name: "manaCrystal"
+            },
+            {
+                amount: 40,
+                name: "yellowCoin"
+            }
+        ]
+    },
+    airEther: {
+        name: "Air Ether",
+        prodTime: 10,
+        ingredients: [
+            {
+                amount: 1,
+                name: "airStone"
+            },
+            {
+                amount: 2,
+                name: "manaPower"
+            }
+        ]
+    },
+    cloth: {
+        name: "Cloth",
+        prodTime: 4,
+        ingredients: [
+            {
+                amount: 1,
+                name: "cotton"
+            }
+        ]
+    },
+    bread: {
+        name: "Bread",
+        prodTime: 4,
+        ingredients: [
+            {
+                amount: 2,
+                name: "flour"
+            },
+            {
+                amount: 1,
+                name: "fuel"
+            }
+        ]
+    },
+    wool: {
+        name: "Wool",
+        prodTime: 6,
+        ingredients: [
+            {
+                amount: 2,
+                name: "animalFeed"
+            },
+            {
+                amount: 2,
+                name: "water"
+            }
+        ]
+    }
+}
+
+export default recipes;
